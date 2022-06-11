27cebe58c6ef92762949522e1f87b24f5c84e77f
Praveen kusuluri <praveenkusuluri07@gmail.com>
2022-06-11 20:39:34 +0530

    Signin route for token auth

--- controllers/schemas/user.js
+++ controllers/schemas/user.js
@@ -1,36 +1,73 @@
-const {SignUp} = require("../handlers/userHandler")
+const { SignUp, SignIn } = require("../handlers/userHandler")
 const joi = require("joi")
 
-const signUpOptions ={
-    schema:{
-        tags:["User"],
-        response:{
-            201:{
-                type:'object',
-                properties:{
-                    message:{type:"string"},
-                    UserDate:{
-                        type:"object",
-                        properties:{
-                            _id:{type:"string",example:"some document id"},
-                            email:{type:"string",example:"example@domain.com"},
-                            firstName:{type:"string",example:"user_name"},
-                            token:{type:"string",example:"token"}
-                        }
-                    }
-                }
+const signUpOptions = {
+  schema: {
+    tags: ["User"],
+    response: {
+      201: {
+        type: "object",
+        properties: {
+          message: { type: "string" },
+          UserDate: {
+            type: "object",
+            properties: {
+              _id: { type: "string", example: "some document id" },
+              email: { type: "string", example: "example@domain.com" },
+              firstName: { type: "string", example: "user_name" },
+              token: { type: "string", example: "token" },
             },
-            404:{
-                type:"object",
-                properties:{
-                    error:{type:"string"},
-                    message:{type:"string"},
-                    statusCode:{type:"number"}
-                }
-            }
-        }
+          },
+        },
+      },
+      404: {
+        type: "object",
+        properties: {
+          error: { type: "string" },
+          message: { type: "string" },
+          statusCode: { type: "number" },
+        },
+      },
     },
-    handler:SignUp
+  },
+  handler: SignUp,
 }
 
-module.exports={signUpOptions}
+const signinOptions = {
+  schema: {
+    tags: ["User"],
+    summary: "User signin to the application",
+    body: {
+      type: "object",
+      properties: {
+        email: { type: "string", example: "example@domain.com" },
+        password: { type: "string", example: "123456" },
+      },
+    },
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          token: { type: "string" },
+          user: {
+            type: "object",
+            properties: {
+              _id: { type: "string" },
+              email: { type: "string" },
+              firstName: { type: "string" },
+            },
+          },
+        },
+      },
+      404:{
+        type:"object",
+        properties:{
+            message:{type:"string"}
+        }
+      }
+    },
+  },
+  handler:SignIn
+}
+
+module.exports = { signUpOptions, signinOptions }
